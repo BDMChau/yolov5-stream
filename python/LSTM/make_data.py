7cac4f6de8fbee1f3fcfb1b4b0b06768969ca82b
chau <bdmchau105@gmail.com>
2024-03-24 20:03:56 +0700

    make data from imgs folder

--- python/LSTM/make_data.py
+++ python/LSTM/make_data.py
@@ -14,10 +14,10 @@ print(f"Using device: {device}")
 modelPersonPose = YOLO("./weights/yolov8s-pose.pt").to(device)
 
 
-def make_data_from_video():
+def make_data_from_video(path):
     data_to_write = []
 
-    cap = cv2.VideoCapture(parent_dir + "/data/stealing.mp4")
+    cap = cv2.VideoCapture(path)
 
     while cap.isOpened():
         ret, img = cap.read()
@@ -33,9 +33,6 @@ def make_data_from_video():
         )
 
         for r in modelPersonPoseResults:
-            if r.boxes is None:
-                continue
-
             img = r.plot(kpt_line=True, kpt_radius=5)
  
             # POSE
@@ -67,5 +64,55 @@ def make_data_from_video():
     cv2.destroyAllWindows()
 
 
+def make_data_from_imgs(folder_path):
+    files = os.listdir(folder_path)
+    image_files = [file for file in files if file.lower().endswith((".jpg", ".jpeg", ".png"))]
+
+    data_to_write = []
+    for image_file in image_files:
+        image_path = os.path.join(folder_path, image_file)
+
+        img = cv2.imread(image_path)
+
+        if img is not None:
+            print(f"image OK: {image_path}")
+            modelPersonPoseResults = modelPersonPose(
+                img,
+                stream=True,
+                conf=0.2
+            )
+
+            for r in modelPersonPoseResults:
+                img = r.plot(kpt_line=True, kpt_radius=5)
+    
+                # POSE
+                keypoints_xyn = r.keypoints.xyn[0]  # just get the first
+                result_timestep = []
+                for i, keypoint_tensor in enumerate(keypoints_xyn):
+                    x, y = (
+                        keypoint_tensor[0].item(),
+                        keypoint_tensor[1].item(),
+                    )
+
+                    result_timestep.append(x)
+                    result_timestep.append(y)
+
+                data_to_write.append(result_timestep)
+                
+                cv2.imshow("image", img)
+                cv2.waitKey(1)
+        else:
+            print(f"Failed to load the image: {image_path}")
+            
+     # save to txt file
+    print("Writing file...")
+    file_name = "stealing"
+    df = pd.DataFrame(data_to_write)
+    df.to_csv("files/" + file_name + ".txt")
+    cv2.destroyAllWindows()
+
+    
+
 if __name__ == "__main__":
-    make_data_from_video()
+    make_data_from_video(parent_dir + "/data/stealing.mp4")
+    # make_data_from_imgs(parent_dir + "/data/imgs")
